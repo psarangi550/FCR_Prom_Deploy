f476b4a6573d03c636335855f28710b0e87aaf3d
pratik-kumar.sarangi <pratik-kumar.sarangi@siemens.com>
2023-06-06 21:07:31 +0530

    updating the api endpoint

--- resources.py
+++ resources.py
@@ -6,34 +6,55 @@ from json import dumps
 
 ns=Namespace("api")
 
+def generate_instant_metrics():
+	data={
+		"payload": {
+			"status": "success",
+			"data": {
+				"resultType": "matrix",
+				"result": []
+			}
+		},
+		"_msgid": "b5bcaec7930b8fa2",
+		"startTime": time.strftime("%Y:%m:%D %H:%m:%S",time.localtime(time.time()-30)),
+		"endTime": time.strftime("%Y:%m:%D %H:%m:%S",time.localtime(time.time())),
+		"query": "pipeline_pipeline_preprocessor_exec_avg",
+		"statusCode": 200,
+		"headers": {
+			"content-type": "application/json",
+			"date": datetime.isoformat(datetime.now()),
+			"content-length": "63",
+			"connection": "close",
+			"x-node-red-request-node": "cf15688a"
+		},
+		"responseUrl": "http://prometheus:9090/api/v1/query_range?query=pipeline_pipeline_preprocessor_exec_avg{device_name=%22fcrvirt427e8%22}&start=1686031854.809&end=1686031884.809&step=45",
+		"redirectList": [],
+		"retry": 0
+	}
+	return data
 
-data={
-	"payload": {
-		"status": "success",
-		"data": {
-			"resultType": "matrix",
-			"result": []
-		}
-	},
-	"_msgid": "b5bcaec7930b8fa2",
-	"startTime": time.strftime("%Y:%m:%D %H:%m:%S",time.localtime(time.time()-30)),
-	"endTime": time.strftime("%Y:%m:%D %H:%m:%S",time.localtime(time.time())),
-	"query": "pipeline_pipeline_preprocessor_exec_avg",
-	"statusCode": 200,
-	"headers": {
-		"content-type": "application/json",
-		"date": datetime.isoformat(datetime.now()),
-		"content-length": "63",
-		"connection": "close",
-		"x-node-red-request-node": "cf15688a"
-	},
-	"responseUrl": "http://prometheus:9090/api/v1/query_range?query=pipeline_pipeline_preprocessor_exec_avg{device_name=%22fcrvirt427e8%22}&start=1686031854.809&end=1686031884.809&step=45",
-	"redirectList": [],
-	"retry": 0
-}
+
+
+def generate_aggregated_metrics():
+    result=[]
+    for _ in range(18):
+        time.sleep(0.1)
+        result.append(generate_instant_metrics())
+    return result
+    
 
 @ns.route("/promapi")
 class Home(Resource):
     
     def get(self):
+        data=generate_instant_metrics()
         return {"data":data}
+    
+    
+
+@ns.route("/aggregate")
+class AggreGateMetrics(Resource):
+    
+    def get(delf):
+        aggregate=dumps(generate_aggregated_metrics())
+        return {"aggregated_result":aggregate}
