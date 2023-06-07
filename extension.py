5452542648c36e27a1f762b138892ae2fd96b326
pratik-kumar.sarangi <pratik-kumar.sarangi@siemens.com>
2023-06-07 08:57:45 +0530

    renaming the app

--- extension.py
+++ extension.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 
-api=Api(version='1.0', title='FCR Prometheus API ',description="Simulates FCR Prometheus API exposing metrics")
+api=Api(version='1.0', title='FCR Prometheus API Simulation ',description="Simulates FCR Prometheus API exposing metrics")
 db=SQLAlchemy()
